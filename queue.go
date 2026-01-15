97432d4971558d28b4da42366cfcff9ccbd1ed96
Ideadly-Skies <Obie.kal22@gmail.com>
2026-01-15 10:17:34 +0700

    refactor(deadlock): fix deadlock in goroutine

--- queue.go
+++ queue.go
@@ -13,40 +13,38 @@ type Message struct {
 }
 
 type InMemoryQueue struct {
-	messages []Message
-	mu       sync.Mutex
-	cond     *sync.Cond
+	subscribers []chan Message
+	mu          sync.RWMutex
 }
 
 func NewQueue() *InMemoryQueue {
-	q := &InMemoryQueue{
-		messages: make([]Message, 0),
+	return &InMemoryQueue{
+		subscribers: make([]chan Message, 0),
 	}
-	q.cond = sync.NewCond(&q.mu)
-	return q
 }
 
 func (q *InMemoryQueue) Publish(msg Message) {
-	q.mu.Lock()
-	defer q.mu.Unlock()
-	q.messages = append(q.messages, msg)
-	q.cond.Signal() // Wake up waiting subscriber
+	q.mu.RLock()
+	defer q.mu.RUnlock()
+
+	// Send to all subscribers
+	for _, sub := range q.subscribers {
+		select {
+		case sub <- msg:
+			// Message sent successfully
+		default:
+			// Skip if subscriber's channel is full
+		}
+	}
 }
 
 func (q *InMemoryQueue) Subscribe() <-chan Message {
 	ch := make(chan Message, 10)
-	go func() {
-		for {
-			q.mu.Lock()
-			for len(q.messages) == 0 {
-				q.cond.Wait() // Wait for new messages
-			}
-			msg := q.messages[0]
-			q.messages = q.messages[1:]
-			q.mu.Unlock()
-			ch <- msg
-		}
-	}()
+
+	q.mu.Lock()
+	q.subscribers = append(q.subscribers, ch)
+	q.mu.Unlock()
+
 	return ch
 }
 
--- subscriber.go
+++ subscriber.go
@@ -15,12 +15,20 @@ func runSubscriber() {
 	msgChan := messageQueue.Subscribe()
 	msgCount := 0
 
-	for msg := range msgChan {
-		msgCount++
-		fmt.Printf("✓ Received message #%d:\n", msgCount)
-		fmt.Printf("  ID: %d\n", msg.ID)
-		fmt.Printf("  Content: %s\n", msg.Content)
-		fmt.Printf("  Published: %s\n", msg.Timestamp.Format(time.RFC3339))
-		fmt.Printf("  Received: %s\n\n", time.Now().Format(time.RFC3339))
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case msg := <-msgChan:
+			msgCount++
+			fmt.Printf("✓ Received message #%d:\n", msgCount)
+			fmt.Printf("  ID: %d\n", msg.ID)
+			fmt.Printf("  Content: %s\n", msg.Content)
+			fmt.Printf("  Published: %s\n", msg.Timestamp.Format(time.RFC3339))
+			fmt.Printf("  Received: %s\n\n", time.Now().Format(time.RFC3339))
+		case <-ticker.C:
+			fmt.Println("⏱️  Still listening... (waiting for messages)")
+		}
 	}
 }
